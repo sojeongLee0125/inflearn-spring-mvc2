72216d4b68c25881aa3f2f12fb7a46126c749e79
sojeonglee <sojeonglee0125@gmail.com>
2022-11-09 21:19:09 +0900

    feat : 타임리프 - 체크박스 (히든필드 사용)

--- src/main/java/hello/thymeleaf/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/thymeleaf/itemservice/web/form/FormItemController.java
@@ -3,6 +3,7 @@ package hello.thymeleaf.itemservice.web.form;
 import hello.thymeleaf.itemservice.domain.item.Item;
 import hello.thymeleaf.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.util.List;
 
 @Controller
+@Slf4j
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
 public class FormItemController {
@@ -44,6 +46,7 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        log.info("item.open={}", item.getOpen());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
